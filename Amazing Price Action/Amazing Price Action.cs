ede0390fccae780555ec32652170337b3ee58a38
Leonardo Ciaccio <leonardo.ciaccio@gmail.com>
2023-03-09 09:21:40 +0100

    1.0.4 Signal

--- Amazing Price Action/Amazing Price Action.cs
+++ Amazing Price Action/Amazing Price Action.cs
@@ -1,4 +1,4 @@
-﻿/*  CTRADER GURU --> Indicator Template 1.0.8
+/*  CTRADER GURU --> Indicator Template 1.0.8
 
     Homepage    : https://ctrader.guru/
     Telegram    : https://t.me/ctraderguru
@@ -10,6 +10,7 @@
 */
 
 using System;
+using System.Threading.Tasks;
 using cAlgo.API;
 using cAlgo.API.Internals;
 
@@ -59,7 +60,7 @@ namespace cAlgo
 
     #endregion
 
-    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.FullAccess)]
     public class AmazingPriceAction : Indicator
     {
 
@@ -79,7 +80,7 @@ namespace cAlgo
 
         public const string NAME = "Amazing Price Action";
 
-        public const string VERSION = "1.0.2";
+        public const string VERSION = "1.0.4";
 
         #endregion
 
@@ -88,14 +89,41 @@ namespace cAlgo
         [Parameter(NAME + " " + VERSION, Group = "Identity", DefaultValue = "https://www.google.com/search?q=ctrader+guru+amazing+price+action")]
         public string ProductInfo { get; set; }
 
-        [Parameter("Period", Group = "Params", DefaultValue = 20, MinValue = 1)]
+        [Parameter("Period", Group = "Params", DefaultValue = 3, MinValue = 1)]
         public int Period { get; set; }
 
         [Parameter("Mode", Group = "Params", DefaultValue = Mode.Close)]
         public Mode Moden { get; set; }
 
-        [Output("APA", LineColor = "DodgerBlue")]
+        [Parameter("Show", Group = "Signal", DefaultValue = true)]
+        public bool ShowSignal { get; set; }
+
+        [Parameter("Color Up", Group = "Signal", DefaultValue = "DodgerBlue")]
+        public Color ColorUp { get; set; }
+
+        [Parameter("Color Dw", Group = "Signal", DefaultValue = "Red")]
+        public Color ColorDw { get; set; }
+
+        [Parameter("Enabled?", Group = "Webhook", DefaultValue = false)]
+        public bool WebhookEnabled { get; set; }
+
+        [Parameter("EndPoint", Group = "Webhook", DefaultValue = "https://api.telegram.org/bot[ YOUR TOKEN ]/sendMessage")]
+        public string EndPoint { get; set; }
+        public Webhook MyWebook;
+
+        [Parameter("POST", Group = "Webhook", DefaultValue = "chat_id=[ @CHATID ]&text={0}")]
+        public string PostParams { get; set; }
+
+        [Output("APA", LineColor = "Gray", PlotType = PlotType.DiscontinuousLine, Thickness = 1)]
         public IndicatorDataSeries Result { get; set; }
+
+        [Output("APA Up", LineColor = "DodgerBlue", PlotType = PlotType.DiscontinuousLine, Thickness = 1)]
+        public IndicatorDataSeries ResultUp { get; set; }
+
+        [Output("APA Down", LineColor = "Red", PlotType = PlotType.DiscontinuousLine, Thickness = 1)]
+        public IndicatorDataSeries ResultDw { get; set; }
+
+        int LastDirection = 0;
 
         #endregion
 
@@ -106,11 +134,40 @@ namespace cAlgo
 
             Print("{0} : {1}", NAME, VERSION);
 
+            if (!WebhookEnabled)
+                return;
+
+            EndPoint = EndPoint.Trim();
+            if (EndPoint.Length < 1)
+            {
+
+                MessageBox.Show("Wrong 'EndPoint', es. 'https://api.telegram.org/bot[ YOUR TOKEN ]/sendMessage', the service will be disabled.", "Error",MessageBoxButton.OK,MessageBoxImage.Error);
+                WebhookEnabled = false;
+                return;
+
+            }
+
+            PostParams = PostParams.Trim();
+            if (PostParams.IndexOf("{0}") < 0)
+            {
+
+                MessageBox.Show("Wrong 'POST params', es. 'chat_id=[ @CHATID ]&text={0}' there must be the parameter '{0}', the service will be disabled.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                WebhookEnabled = false;
+                return;
+
+            }
+
+            MyWebook = new Webhook(EndPoint);
+            // --> ToWebhook(string.Format("{0}: APA | {1} | Activated", SymbolName, TimeFrame.ToString()), true);
+
         }
 
         public override void Calculate(int index)
         {
 
+            ResultUp[index] = double.NaN;
+            ResultDw[index] = double.NaN;
+            
             if (index > Period)
             {
 
@@ -121,19 +178,56 @@ namespace cAlgo
                 {
 
                     Result[index] = lower;
+                    ResultUp[index] = lower;
+                    ResultUp[index-1] = Result[index-1];
+
+                    if (ShowSignal && LastDirection != 1 ) {
+
+                        ChartIcon IconUp = Chart.DrawIcon("Up" + index, ChartIconType.Circle, index, Bars.OpenPrices[index], ColorUp);
+                        IconUp.IsInteractive = false;
+                        IconUp.IsLocked = true;
+                        ToWebhook(string.Format("{0}: APA | {1} | Buy signal", SymbolName, TimeFrame.ToString()));
+
+                    }
+                    
+                    LastDirection = 1;
 
                 }
                 else if (Bars.OpenPrices[index] < Result[index - 1] && upper < Result[index - 1])
                 {
 
                     Result[index] = upper;
+                    ResultDw[index] = upper;
+                    ResultDw[index-1] = Result[index-1];
+
+
+                    if (ShowSignal && LastDirection != -1)
+                    {
+
+                        ChartIcon IconDw = Chart.DrawIcon("Dw" + index, ChartIconType.Circle, index, Bars.OpenPrices[index], ColorDw);
+                        IconDw.IsInteractive = false;
+                        IconDw.IsLocked = true;
+                        ToWebhook(string.Format("{0}: APA | {1} | Sell signal", SymbolName, TimeFrame.ToString()));
+
+                    }
+                    LastDirection = -1;
 
                 }
                 else
                 {
 
                     Result[index] = Result[index - 1];
+                    
+                    if(LastDirection == 1){
 
+                        ResultUp[index] = Result[index];
+        
+                    }else if(LastDirection == -1){
+            
+                        ResultDw[index] = Result[index];
+            
+                    }
+                    
                 }
 
             }
@@ -143,6 +237,24 @@ namespace cAlgo
                 Result[index] = Bars.OpenPrices[index];
 
             }
+
+        }
+
+        private void ToWebhook(string mex, bool force = false)
+        {
+
+            bool canSendMessage = RunningMode == RunningMode.RealTime;
+
+            if ((!IsLastBar && !force) || !canSendMessage || MyWebook == null || !WebhookEnabled || mex == null || mex.Trim().Length == 0)
+                return;
+
+            mex = mex.Trim();
+
+            Task<Webhook.WebhookResponse> webhook_result = Task.Run(async () => await MyWebook.SendAsync(string.Format(PostParams, mex)));
+
+            // --> We don't know which webhook the client is using, probably a json response
+            // --> var Telegram = JObject.Parse(webhook_result.Result.Response);
+            // --> Print(Telegram["ok"]);
 
         }
 
---
+++ Amazing Price Action/ToWebhook.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+
+namespace cAlgo
+{
+
+    public class Webhook
+    {
+
+
+        /* --> USAGE:
+              
+        [Parameter("EndPoint", Group = "Webhook", DefaultValue = "https://api.telegram.org/bot[ YOUR TOKEN ]/sendMessage")]
+        public string EndPoint { get; set; }
+        public Webhook MyWebook;
+
+        [Parameter("POST", Group = "Webhook", DefaultValue = "chat_id=[ @CHATID ]&text={0}")]
+        public string PostParams { get; set; }
+
+        public Webhook MyWebook;
+
+        protected override void OnStart()
+        {
+
+            Print("{0} : {1}", NAME, VERSION);
+
+            EndPoint = EndPoint.Trim();
+            if (EndPoint.Length < 1)
+            {
+
+                MessageBox.Show("Wrong 'EndPoint', es. 'https://api.telegram.org/bot[ YOUR TOKEN ]/sendMessage'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Stop();
+
+            }
+
+            PostParams = PostParams.Trim();
+            if (PostParams.IndexOf("{0}") < 0)
+            {
+
+                MessageBox.Show("Wrong 'POST params', es. 'chat_id=[ @CHATID ]&text={0}'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Stop();
+
+            }
+
+            MyWebook = new Webhook(EndPoint);
+
+        }
+
+        private void ToWebhook(Position position, string message)
+        {
+
+            if (message.Trim().Length == 0)
+                return;
+
+            if (OnlyThis && position.SymbolName != SymbolName)
+                return;
+
+            bool nolabel = position.Label == null || position.Label.Length == 0;
+
+            if (ListLabels.Count > 0 && (nolabel || ListLabels.IndexOf(position.Label) < 0))
+                return;
+
+            message = message.Trim();
+
+            string message_to_send = FormatMessage(position, message);
+
+            Task<Webhook.WebhookResponse> webhook_result = Task.Run(async () => await MyWebook.SendAsync(string.Format(PostParams, message_to_send)));
+
+            // --> We don't know which webhook the client is using, probably a json response
+            // --> var Telegram = JObject.Parse(webhook_result.Result.Response);
+            // --> Print(Telegram["ok"]);
+
+        }
+         
+        */
+
+
+
+        public class WebhookResponse
+        {
+
+            public int Error { get; set; }
+            public string Response { get; set; }
+
+        }
+
+        private readonly string EndPoint = "";
+
+        public Webhook(string NewEndPoint)
+        {
+
+            if (NewEndPoint.Trim().Length < 1) throw new ArgumentException("Parameter cannot be null", "NewEndPoint");
+
+            EndPoint = NewEndPoint.Trim();
+
+        }
+
+        public async Task<WebhookResponse> SendAsync(string post_params)
+        {
+
+            WebhookResponse response = new WebhookResponse();
+
+            try
+            {
+               
+                HttpClient client = new();
+                HttpResponseMessage responsej = await client.GetAsync(EndPoint + "?" + post_params);
+
+                responsej.EnsureSuccessStatusCode();
+                string responseBody = await responsej.Content.ReadAsStringAsync();
+
+                response.Response = responseBody;
+                response.Error = 0;
+                return response;
+
+            }
+            catch (Exception exc)
+            {
+
+                response.Response = exc.Message;
+                response.Error = 1;
+                return response;
+
+            }
+
+        }
+
+    }
+
+}
